b48193fc90dc2e21288bbaec71003e417295f554
abdelrmamsaahlan-max <abdelrmamsaahlan@gmail.com>
2026-09-03 21:30:46 +0300

    Remove mutation bases / explore base paints section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Features from '@/components/Features';
 import ServicesShowcase from '@/components/ServicesShowcase';
-import BasesWePaint from '@/components/BasesWePaint';
 import HowItWorks from '@/components/HowItWorks';
 import DiscordStatus from '@/components/DiscordStatus';
 import Community from '@/components/Community';
@@ -27,7 +26,6 @@ export default function App() {
         <Hero />
         <Features />
         <ServicesShowcase />
-        <BasesWePaint />
         <HowItWorks />
         <DiscordStatus />
         <Community />
