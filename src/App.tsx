85d3561bac1a0c600493b648b90b57a5f4d2df1a
abdelrmamsaahlan-max <abdelrmamsaahlan@gmail.com>
2026-09-03 21:22:26 +0300

    Add Base Skins section to FSMM website

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Features from '@/components/Features';
 import ServicesShowcase from '@/components/ServicesShowcase';
+import BaseSkins from '@/components/BaseSkins';
 import HowItWorks from '@/components/HowItWorks';
 import DiscordStatus from '@/components/DiscordStatus';
 import Community from '@/components/Community';
@@ -26,6 +27,7 @@ export default function App() {
         <Hero />
         <Features />
         <ServicesShowcase />
+        <BaseSkins />
         <HowItWorks />
         <DiscordStatus />
         <Community />
